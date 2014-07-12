ec621818005bd8c57972a5d5b5dfa40ecdd11a4e
ops-dev-sh <ops-dev-sh@users.noreply.github.com>
2014-07-12 13:22:43 +0800

    update diff previous version and amend commit

--- recipes/git.sh
+++ recipes/git.sh
@@ -20,10 +20,16 @@ alias=~/.bash_aliases
 [ -f $alias ] || touch $alias
 
 #for git
-block_append 'alias gs="git status "' $alias
-block_append 'alias ga="git add "' $alias
-block_append 'alias gc="git commit -a -m "' $alias
-block_append 'alias gd="git diff "' $alias
+block_append 'alias gs="git status"' $alias
+block_append 'alias ga="git add"' $alias
+
+block_append 'alias gc="git commit -a -m"' $alias
+block_append 'alias gc-amend="git commit --amend"' $alias
+
+block_append 'alias gd="git diff"' $alias
+block_append 'alias gd-previous="git diff HEAD@{1}"' $alias
+
 block_append 'alias pull="git pull"' $alias
 block_append 'alias push="git push"' $alias
+
 block_append 'alias gl="git log --oneline"' $alias
