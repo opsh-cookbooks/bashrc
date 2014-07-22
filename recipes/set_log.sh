5d0bdd6c98111dbe927c9218691f2d4e0fa3da81
ops-dev-sh <ops-dev-sh@users.noreply.github.com>
2014-07-22 15:21:07 +0800

    setlog use global and add logrotate

--- recipes/set_log.sh
+++ recipes/set_log.sh
@@ -13,26 +13,29 @@
 #environment="SSH_USER=USER1" ssh-rsa AAAAfgds...
 #environment="SSH_USER=USER2" ssh-rsa AAAAukde..
 
-
 #prepare log directory
 
-$sudo block_insert "PermitUserEnvironment yes" /etc/ssh/sshd_config
-
+global=true
 log_path=/var/log/bash_history
 
+#make sure enable environment
+block_insert "PermitUserEnvironment yes" /etc/ssh/sshd_config
+
+#prepare log directory
 set_permission() {
   mkdir $log_path
-  chmod 0777 $log_path
+  chmod 0757 $log_path
   chattr -R +a $log_path
 }
 [ ! -d $log_path ] && $sudo set_permission
 
-#todo: check exist and permission
 
-template_cp "bash_setlog.sh" ~/.bash_setlog
-
-#display date of history in ISO 8601
-block_append 'export HISTTIMEFORMAT="%FT%T%z "' ~/.bashrc
-block_append 'export HISTIGNORE="&:ls:[bf]g:exit"' ~/.bashrc
-
-block_append '[ -f ~/.bash_setlog ] && . ~/.bash_setlog' ~/.bashrc
+if [ "global" == "true" ]; then
+    template_cp "bash_history.logrotate" /etc/logrotate.d/bash_history
+    template_cp "bash_setlog.sh" /etc/bash.setlog
+    block_append '[ -f /etc/bash.setlog ] && . /etc/bash.setlog' /etc/bash.bashrc
+else
+    echo "only use global"
+    #template_cp "bash_setlog.sh" ~/.bash_setlog
+    #block_append '[ -f ~/.bash_setlog ] && . ~/.bash_setlog' ~/.bashrc
+fi
--- templates/bash_setlog.sh
+++ templates/bash_setlog.sh
@@ -1,6 +1,16 @@
+#log multiline log
 shopt -s cmdhist
+
+#display date of history in ISO 8601
+export HISTTIMEFORMAT="%FT%T%z "
+#ignore ls, fg, bg, exit, history
+export HISTIGNORE="&:ls:[bf]g:exit[ ]:history[ ]"
+
+#change log file path if SSH_USER exists
 if [ "$SSH_USER" != "" ]; then
   readonly HISTFILE="/var/log/bash_history/${SSH_USER}_history.log"
   [ ! -f $HISTFILE ] && touch $HISTFILE
-  readonly PROMPT_COMMAND="history -a;$PROMPT_COMMAND"
 fi
+
+#write to log, refresh
+readonly PROMPT_COMMAND="history -a; history -c; history -r;$PROMPT_COMMAND"
